03b2fc08862747a6b2686888ac9eec60c9911e01
unknown <Sistemas-Simit@sitemas-cam.movilidadvicio.local>
2021-07-26 14:43:04 -0500

    form promedio y funcion promedio listos

--- promedio/promedio.js
+++ promedio/promedio.js
@@ -1,3 +1,5 @@
+var lista=[]
+
 function obtenerLista(lista){
     const cadenaSinEspacios=lista.trim()
 var arrayCadenas= cadenaSinEspacios.split(",")
@@ -18,7 +20,7 @@ for (var i=0; i < arrayCadenas.length; i++) {
 }
 
 function promedio(){
-lista=ejecutarObtenerLista();
+var lista=ejecutarObtenerLista();
 const suma=lista.reduce(function(inicio=0,siguiente){
 return inicio+siguiente;
 });
@@ -30,4 +32,54 @@ return promedio;
 function clearTextarea(){
     var clear=document.getElementById("lista")
     clear.innerHTML='';
+}
+
+function ingresarDato(dato){
+
+lista.push(parseInt(dato));
+return lista;
+}
+
+function datos(){
+    var lista2=document.getElementById("lista2")
+    var datonuevo=lista2.value
+    var ingresarDat=ingresarDato(datonuevo);
+    console.log(ingresarDat)
+    clearinput();
+    return ingresarDat
+}
+
+function calcularUnoPorUno(){
+    
+    if(lista.length>0){
+    var sumaLista=lista.reduce(function(inicio=0,siguiente){
+        return inicio+siguiente;
+        });
+        const promedio=sumaLista/lista.length;
+console.log(promedio);
+
+} else{
+    alert("ingresa un numero")
+}
+
+return promedio;
+}
+
+function clearinput(){
+    var lista2=document.getElementById("lista2")
+ 
+    lista2.value='';
+    lista2.focus();
+}
+
+function checkboxAble(){
+    var buttonCalcInUnoAuno= document.getElementById("buttonCalcInUnoAuno");
+    var buttonInUnoAuno=document.getElementById("buttonInUnoAuno");
+    var checkboxUnoPoruno = document.getElementById("checkboxUnoPoruno");
+    var lista2= document.getElementById("lista2");
+    checkboxUnoPoruno.onchange= function() {
+        buttonInUnoAuno.disabled = !this.checked;
+        buttonCalcInUnoAuno.disabled= !this.checked;
+        lista2.disabled= !this.checked;
+    };
 }
