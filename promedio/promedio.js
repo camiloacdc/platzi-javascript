4c384fc18a9cce4f7c8476ad95d23f13033e90c6
unknown <Sistemas-Simit@sitemas-cam.movilidadvicio.local>
2021-07-27 15:19:15 -0500

    falta una funcion de moda,falta obtener uno por uno

--- promedio/promedio.js
+++ promedio/promedio.js
@@ -1,5 +1,6 @@
 var lista = [];
-
+var listaMediana=[];
+var listaModa=[];
 function obtenerLista(lista) {
   const cadenaSinEspacios = lista.trim();
   var arrayCadenas = cadenaSinEspacios.split(",");
@@ -97,8 +98,20 @@ function checkboxAble() {
   };
 }
 
+function ejecutarObtenerListaMediana() {
+  const lista = document.getElementById("listaMediana");
+  const listaValue = lista.value;
+  var arrayCadenas = obtenerLista(listaValue);
+  var listaFinal = [];
+  for (var i = 0; i < arrayCadenas.length; i++) {
+    listaFinal.push(parseInt(arrayCadenas[i]));
+  }
+  console.log(listaFinal);
+  return listaFinal;
+}
+
 function ordenarListaSort() {
-  var lista = ejecutarObtenerLista();
+  var lista = ejecutarObtenerListaMediana();
   var listaOrdenada = lista.sort(function (a, b) {
     return a - b;
   });
@@ -108,7 +121,7 @@ function ordenarListaSort() {
 function ordenarListafor() {
   var tempVar;
   var listaOrdenada = [];
-  var lista = ejecutarObtenerLista();
+  var lista = ejecutarObtenerListaMediana();
 
   for (i = 0; i < lista.length; i++) {
     for (j = i; j < lista.length; j++) {
@@ -121,4 +134,159 @@ function ordenarListafor() {
     listaOrdenada.push(lista[i]);
   }
   return listaOrdenada;
+}
+function calcularMediana(){
+  var listaOr=ordenarListaSort()
+  var tamano=listaOr.length;
+  var mediana;
+  var n1,n2;
+if (tamano % 2==0){
+   n1=listaOr[parseInt(listaOr.length/2)-1]
+  n2=listaOr[parseInt(listaOr.length/2)]
+  mediana=(n1+n2)/2
+}
+else {
+  mediana=listaOr[parseInt(listaOr.length/2)];
+}
+alert(mediana) ;
+}
+function datosMediana() {
+  var lista2 = document.getElementById("lista2Mediana");
+  var datonuevo = lista2.value;
+  var ingresarDat = ingresarDato(datonuevo);
+  console.log(ingresarDat);
+  clearinput();
+  return ingresarDat;
+}
+
+
+function checkboxAbleMediana() {
+  var buttonCalcInUnoAunoMediana = document.getElementById("buttonCalcInUnoAunoMediana");
+  var buttonInUnoAunoMediana = document.getElementById("buttonInUnoAunoMediana");
+  var checkboxUnoPorunoMediana = document.getElementById("checkboxUnoPorunoMediana");
+  var lista2Mediana = document.getElementById("lista2Mediana");
+  checkboxUnoPorunoMediana.onchange = function () {
+    buttonInUnoAunoMediana.disabled = !this.checked;
+    buttonCalcInUnoAunoMediana.disabled = !this.checked;
+    lista2Mediana.disabled = !this.checked;
+  };
+}
+
+function ingresarDatoMediana(dato) {
+  listaMediana.push(parseInt(dato));
+  return listaMediana;
+}
+
+function datosMediana() {
+  var lista2 = document.getElementById("lista2Mediana");
+  var datonuevo = lista2.value;
+  var ingresarDat = ingresarDatoMediana(datonuevo);
+  console.log(ingresarDat);
+  clearinputMediana();
+  return ingresarDat;
+}
+
+
+function calcularUnoPorUnoMediana() {
+  var listaMedianaOrdenada;
+  var tamano=listaMediana.length;
+  var mediana;
+  var n1,n2;
+  if (tamano > 0) {
+ listaMedianaOrdenada=listaMediana.sort(function (a, b) {
+  return a - b;
+   
+});
+alert(listaMedianaOrdenada)
+if (tamano % 2==0){
+   n1=listaMedianaOrdenada[parseInt(listaMedianaOrdenada.length/2)-1]
+  n2=listaMedianaOrdenada[parseInt(listaMedianaOrdenada.length/2)]
+  mediana=(n1+n2)/2
+}
+else {
+  mediana=listaMedianaOrdenada[parseInt(listaMedianaOrdenada.length/2)];
+}
+alert(mediana) ;
+  } else {
+    alert("ingresa un numero");
+  }
+
+  return promedio;
+}
+function clearinputMediana() {
+  var lista2Mediana = document.getElementById("lista2Mediana");
+
+  lista2Mediana.value = "";
+  lista2Mediana.focus();
+}
+
+function ejecutarObtenerListaMediana() {
+  const lista = document.getElementById("listaMediana");
+  const listaValue = lista.value;
+  var arrayCadenas = obtenerLista(listaValue);
+  var listaFinal = [];
+  for (var i = 0; i < arrayCadenas.length; i++) {
+    listaFinal.push(parseInt(arrayCadenas[i]));
+  }
+  console.log(listaFinal);
+  return listaFinal;
+}
+
+function ejecutarObtenerListaModa() {
+  const lista = document.getElementById("listaModa");
+  const listaValue = lista.value;
+  var arrayCadenas = obtenerLista(listaValue);
+  var listaFinal = [];
+  for (var i = 0; i < arrayCadenas.length; i++) {
+    listaFinal.push(parseInt(arrayCadenas[i]));
+  }
+  console.log(listaFinal);
+  return listaFinal;
+}
+
+function calcularModa()
+{
+  var lista = ejecutarObtenerListaModa();   
+    // Sort the array
+    lista.sort();
+    console.log(lista);  
+    // find the max frequency using linear
+    // traversal
+    let max_count = 1, res = lista[0];
+    let curr_count = 1;
+       
+    for (let i = 1; i < lista.length; i++)
+    {
+        if (lista[i] == lista[i - 1])
+            curr_count++;
+        else
+        {
+            if (curr_count > max_count)
+            {
+                max_count = curr_count;
+                res = lista[i - 1];
+            }
+            curr_count = 1;
+        }
+    }
+    
+    // If last element is most frequent
+    if (curr_count > max_count)
+    {
+        max_count = curr_count;
+        res = lista[n - 1];
+    }
+    console.log(res);
+}
+
+function checkboxAbleModa() {
+  var buttonCalcInUnoAunoModa = document.getElementById("buttonCalcInUnoAunoModa");
+  var buttonInUnoAunoModa = document.getElementById("buttonInUnoAunoModa");
+  var checkboxUnoPorunoModa = document.getElementById("checkboxUnoPorunoModa");
+  var lista2Moda = document.getElementById("lista2Moda");
+  checkboxUnoPorunoModa.onchange = function () {
+    buttonInUnoAunoModa.disabled = !this.checked;
+    buttonCalcInUnoAunoModa.disabled = !this.checked;
+    lista2Moda.disabled = !this.checked;
+  };
 }
