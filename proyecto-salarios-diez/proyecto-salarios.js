28ea6ae8510a9e43bdb9b46dbbaffe32f3a7226f
unknown <Sistemas-Simit@sitemas-cam.movilidadvicio.local>
2021-08-23 15:48:24 -0500

    terminando proyecto

--- proyecto-salarios-diez/proyecto-salarios.js
+++ proyecto-salarios-diez/proyecto-salarios.js
@@ -1,18 +1,19 @@
-var colombia=[]
-var mexico=[]
-var argentina=[]
+var colombia=[{pais:'Colombia',}];
+var mexico=[{pais:'Mexico',}];
+var argentina=[{pais:'Argentina',}];
 
-function validaPais(){
-    var pais= document.getElementById("pais")
-    var valuePais=pais.value
-    var paisValido="Pais valido"
-    if(valuePais.toLowerCase()=="argentina" || valuePais.toLowerCase()=="colombia" || valuePais.toLowerCase()=="mexico" || valuePais.toLowerCase()=="méxico")
-    {
-        paisValido="pais no valido"
-    }
-    console.log(paisValido)
-    return paisValido
-}
+// function validaPais(){
+//     var pais= document.getElementById("pais")
+//     var valuePais=pais.value
+//     var paisValido="Pais valido";
+//     if(valuePais.toLowerCase()!="argentina" && valuePais.toLowerCase()!="colombia" && valuePais.toLowerCase()!="mexico" && valuePais.toLowerCase()!="méxico")
+//     {
+//         paisValido="pais no valido";
+        
+//     }
+//     console.log(paisValido)
+//     return paisValido
+// }
 
 function usarPais(){
     var pais= document.getElementById("pais")
@@ -28,37 +29,70 @@ function usarPais(){
     }
 
 }
-function ingresarPais(){
-    var paisValidado=validaPais();
-    var usaPais;
-    if(paisValidado==="Pais valido"){
-        usaPais=usarPais();
-    }
-    return usaPais;
-}
-function ingresarDato(){
-    var pais=ingresarPais();
-    var salario=document.getElementById("salario");
-    var nombre=document.getElementById("nombre");
-    var salarioValue=salario.value
-    var nombreValue=nombre.value
+// function ingresarPais(){
+    // var paisValidado=usarPais();
+    // var usaPais=usarPais();
+    // if(paisValidado==="Pais valido"){
+    //     usaPais=usarPais();
+    //     console.log("holaaa")
+    // }
+//     return usaPais;
+// }
+function ingresarDato() {
+    var pais1 = usarPais();
 
-    pais.push({
-        name:nombreValue,
-        salario:salarioValue
+    console.log(pais1[0].pais);
+    var salario = document.getElementById("salario");
+    var nombre = document.getElementById("nombre");
+    var salarioValue = salario.value
+    var nombreValue = nombre.value
+    const paisArgentina = document.getElementById("nombre-argentina");
+    const paisMexico = document.getElementById("nombre-mexico")
+    const paiscolombia = document.getElementById("nombre-colombia")
+    const salarioArgentina = document.getElementById("salario-argentina");
+    const salarioMexico = document.getElementById("salario-mexico")
+    const salariocolombia = document.getElementById("salario-colombia")
+    pais1.push({
+        'name': nombreValue,
+        'salario': salarioValue
     })
-    console.log(pais)
-    return pais;
+    console.log(pais1)
+    if (pais1[0].pais == "Argentina") {
+        paisArgentina.innerHTML += "<p>nombre: " + pais1[pais1.length - 1].name + " </p> ";
+        salarioArgentina.innerHTML+= "<p> salario: "+pais1[pais1.length - 1].salario + " </p>";
+    }
+    else if (pais1[0].pais == "Mexico") {
+        paisMexico.innerHTML += "<p>nombre: " + pais1[pais1.length - 1].name + " </p> ";
+        salarioMexico.innerHTML+="<p> salario: "+pais1[pais1.length - 1].salario + " </p>";
+    }
+    else {
+        paiscolombia.innerHTML += "<p>nombre: " + pais1[pais1.length - 1].name + " </p> ";
+        salariocolombia.innerHTML+="<p> salario: "+pais1[pais1.length - 1].salario + " </p>";
+    }
+    return pais1;
 }
 
 function listaSalarios() {
-  var lista = ingresarDato();
-  lista.pop();
-  var listaOrdenada = lista.map(function (persona) {
-    return parseFloat(persona.salario);
-  });
-  console.log(listaOrdenada);
-  return listaOrdenada;
+    //   var lista = ingresarDato();
+    //   lista.pop();
+    const paisCalcular = document.getElementById("pais-calcular");
+    const paisValue = paisCalcular.value
+    let lista;
+    if (paisValue == "Colombia") {
+        lista = colombia;
+    } else if (paisValue == "Mexico") {
+        lista = mexico;
+    } else {
+        lista = argentina;
+    }
+   if (lista.length>1){
+       lista.shift()
+    var listaOrdenada = lista.map(function (persona) {
+        return parseFloat(persona.salario);
+    });
+    console.log(listaOrdenada);
+    return listaOrdenada;
+}else{ alert("hola")}
 }
 
 function ordenarSalarios(){
@@ -110,3 +144,4 @@ var tamano=listaSalarios.length;
 
 // var esto=Object.keys({colombia})[0]
 // console.log(esto)
+console.log(colombia.length)
